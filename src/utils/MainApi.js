36cb32aa52bd5392ab176c96d67ecc7e3d1f8ffc
Yaroshpolk <yaroshevich.vlad00@mail.ru>
2021-09-05 16:50:54 +0300

    fix

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -94,7 +94,7 @@ function getHeaders() {
 }
 
 const mainApi = new MainApi({
-    apiUrl: 'https://yar.movies.api.nomoredomains.monster/',
+    apiUrl: 'https://yar.movies.api.nomoredomains.monster/api',
 });
 
 export default mainApi;
